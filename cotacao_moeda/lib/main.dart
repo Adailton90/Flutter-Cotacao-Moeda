ae2285b9deca402f65aa87642e8d926941d4ed93
Adailton Silva <adailton.silva10@live.com>
2019-07-29 10:13:28 -0300

    inicio

---
+++ cotacao_moeda/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=46a352de";
+
+void main() async{
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["USD"]);
+  runApp(MaterialApp(
+      home: Container(),
+    ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
